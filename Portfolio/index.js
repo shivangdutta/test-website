1cdb3b209426b02b46c21953ac391a7ae69aff12
Shivang Dutta <shivang.dutta@gmail.com>
2024-09-07 13:03:41 +0530

    Minor changes to css

---
+++ Portfolio/index.js
@@ -0,0 +1,11 @@
+// const arr = ['one', 'two', 'three'];
+// const str = 'Hello';
+// const res = arr.includes('one');
+// const variable = arr.includes('on', 'three');
+// const vari = str.includes('ll');
+
+// console.log(res);
+// console.log(variable);
+// console.log(vari);
+
+console.log(true == '');
